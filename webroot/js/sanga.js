2090d30b6cb6dffd4b2db7e7a14e61399b834dd7
rrd <rrd@1108.cc>
2014-11-14 21:47:50 +0530

    refactor

--- webroot/js/sanga.js
+++ webroot/js/sanga.js
@@ -1,11 +1,7 @@
-jQuery(document).ready(function() {
-		jQuery('ul.sf-menu').superfish({
-			pathClass:	'current'
-		});
-	}
-);
-
 $(function() {
 	$( document ).tooltip({position : { my: "right center", at: "right bottom", collision: "flipfit" }});
 	$( "#birth" ).datepicker();
+	$('ul.sf-menu').superfish({
+			pathClass:	'current'
+		});
 });
