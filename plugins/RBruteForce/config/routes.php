be2c55a95ebd735df8faf9dcaeb6df601296fef3
rrd <rrd@1108.cc>
2014-11-29 19:35:10 +0100

    removing files which are source controlled elsewhere

--- plugins/RBruteForce/config/routes.php
+++
@@ -1,6 +0,0 @@
-<?php
-use Cake\Routing\Router;
-
-Router::plugin('RBruteForce', function($routes) {
-	$routes->fallbacks();
-});
--- plugins/RBruteForce/tests/bootstrap.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Test suite bootstrap for RBruteForce.
- */
-// Customize this to be a relative path for embedded plugins.
-// For standalone plugins, this should point at a CakePHP installation.
-require '/home/rrd/public_html/sanga/config/bootstrap.php';
