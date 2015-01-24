ce3f04c478c6a6e39de7ddd5fa3734b2c4f5c68f
rrd <rrd@1108.cc>
2015-01-24 13:14:17 +0100

    adding plugins.php

---
+++ config/plugins.php
@@ -0,0 +1,9 @@
+<?php
+$baseDir = dirname(dirname(__FILE__));
+return [
+    'plugins' => [
+        'DebugKit' => $baseDir . '/vendor/cakephp/debug_kit/',
+        'RBruteForce' => $baseDir . '/vendor/rrd/rbruteforce/',
+
+    ]
+];
