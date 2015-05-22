c31b7cb2ad7419c892ac667d5dd4696bbae66f87
rrd <rrd@1108.cc>
2015-05-22 18:38:22 +0200

    admin group list

---
+++ src/Controller/Admin/GroupsController.php
@@ -0,0 +1,34 @@
+<?php
+namespace App\Controller\Admin;
+
+use App\Controller\AppController;
+
+/**
+ * Groups Controller
+ *
+ * @property App\Model\Table\GroupsTable $Groups
+ */
+class GroupsController extends AppController {
+
+	public function isAuthorized($user = null) {
+		if ($this->request['action'] == 'add' || $this->request['action'] == 'index') {
+			if ($user['role'] >= 9) {
+				return true;
+			}
+			return false;
+		}
+        return true;
+    }
+
+/**
+ * Index method
+ *
+ * @return void
+ */
+	public function index() {
+		$groups = $this->Groups->find()
+								->contain(['Contacts', 'AdminUsers']);
+		$this->set('groups', $this->paginate($groups));
+	}
+
+}
