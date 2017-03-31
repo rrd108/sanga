5b1d48e29914e3a0af935b537093dd02ea3f33cd
rrd <rrd@webmania.cc>
2017-03-31 13:54:26 +0200

    fix #232

--- src/Controller/ImportsController.php
+++ src/Controller/ImportsController.php
@@ -24,88 +24,108 @@ class ImportsController extends AppController
             
             if (in_array($file_mime, ['application/vnd.ms-excel','text/plain','text/csv'])) {
                 $fileData = fread(fopen($this->request->getData('file.tmp_name'), "r"), $this->request->getData('file.size'));
-                $fileData = explode("\n", $fileData);
-                
-                /*data[0]
-                 *name;contactname;zip;address;phone;email;birth;sex;workplace;workplace_zip;
-                 *workplace_address;workplace_phone;workplace_email;
-                 *contactsource_id;comment;skills;
-                 *  groups;
-                 *x;egyebek
-                 */
-                $fields = explode(";", $fileData[0]);
-                $extraField = 'comment';
-                $this->Contacts = TableRegistry::get('Contacts');
-                $this->Zips = TableRegistry::get('Zips');
-                $this->Settings = TableRegistry::get('Settings');
-                
-                array_shift($fileData);     //remove header - field names
-                
-                $errors = $dataArray = [];
-                $imported = $notImported = 0;
-                
-                foreach ($fileData as $i => $row) {
-                    $dataArray[$extraField] = '';
-                    $data = explode(';', $row);
-                    foreach ($fields as $j => $field) {
-                        if (in_array(
-                            $field,
-                            [
-                                'contactname','legalname','zip','address','phone','email',
-                                'birth','sex','workplace','workplace_zip',
-                                'workplace_address','workplace_phone','workplace_email',
-                                'contactsource_id','comment', 'skills', 'groups',
-                                'created'
-                            ]
-                        )) {     //if this is the $field, the column exists in the contacts or its related tables
-                            if ((isset($data[$j]) && $data[$j]) || $field == 'groups') {    //we have defaults for groups so we should handle even if it is empty
-                                switch ($field)
-                                {
-                                    case 'birth':
-                                        $data[$j] = substr($data[$j], 0, 10);
-                                        break;
-                                    case 'zip':
-                                    case 'workplace_zip':
-                                        if (mb_strpos($data[$j], ' ') === false) {
-                                            $data[$j] = $this->Zips->getIdForZip($data[$j]);
-                                        } else {
-                                            $data[$j] = $this->Zips->getIdForZip(explode(' ', $data[$j]));
-                                        }
-                                        $field .= '_id';
-                                        break;
-                                    case 'skills':
-                                        if (mb_strpos($data[$j], ',') === false) {
-                                            $skill_ids = [$data[$j]];
-                                        } else {
-                                            $skill_ids = explode(',', $data[$j]);
-                                        }
-                                        $data[$j] = ['_ids' => $skill_ids];
-                                        break;
-                                    case 'groups':
-                                        $data[$j] = $this->handleGroups($data[$j]);
-                                        break;
+                if (mb_detect_encoding($fileData, 'UTF-8', true)) {
+                    $fileData = explode("\n", $fileData);
+
+                    /*data[0]
+                     *name;contactname;zip;address;phone;email;birth;sex;workplace;workplace_zip;
+                     *workplace_address;workplace_phone;workplace_email;
+                     *contactsource_id;comment;skills;
+                     *  groups;
+                     *x;egyebek
+                     */
+                    $fields = explode(";", $fileData[0]);
+                    $extraField = 'comment';
+                    $this->Contacts = TableRegistry::get('Contacts');
+                    $this->Zips = TableRegistry::get('Zips');
+                    $this->Settings = TableRegistry::get('Settings');
+
+                    array_shift($fileData);     //remove header - field names
+
+                    $errors = $dataArray = [];
+                    $imported = $notImported = 0;
+
+                    foreach ($fileData as $i => $row) {
+                        $dataArray[$extraField] = '';
+                        $data = explode(';', $row);
+                        foreach ($fields as $j => $field) {
+                            if (in_array(
+                                $field,
+                                [
+                                    'contactname',
+                                    'legalname',
+                                    'zip',
+                                    'address',
+                                    'phone',
+                                    'email',
+                                    'birth',
+                                    'sex',
+                                    'workplace',
+                                    'workplace_zip',
+                                    'workplace_address',
+                                    'workplace_phone',
+                                    'workplace_email',
+                                    'contactsource_id',
+                                    'comment',
+                                    'skills',
+                                    'groups',
+                                    'created'
+                                ]
+                            )) {     //if this is the $field, the column exists in the contacts or its related tables
+                                if ((isset($data[$j]) && $data[$j]) || $field == 'groups') {    //we have defaults for groups so we should handle even if it is empty
+                                    switch ($field) {
+                                        case 'birth':
+                                            $data[$j] = substr($data[$j], 0, 10);
+                                            break;
+                                        case 'zip':
+                                        case 'workplace_zip':
+                                            if (mb_strpos($data[$j], ' ') === false) {
+                                                $data[$j] = $this->Zips->getIdForZip($data[$j]);
+                                            } else {
+                                                $data[$j] = $this->Zips->getIdForZip(explode(' ', $data[$j]));
+                                            }
+                                            $field .= '_id';
+                                            break;
+                                        case 'skills':
+                                            if (mb_strpos($data[$j], ',') === false) {
+                                                $skill_ids = [$data[$j]];
+                                            } else {
+                                                $skill_ids = explode(',', $data[$j]);
+                                            }
+                                            $data[$j] = ['_ids' => $skill_ids];
+                                            break;
+                                        case 'groups':
+                                            $data[$j] = $this->handleGroups($data[$j]);
+                                            break;
+                                    }
+                                    $dataArray[$field] = $data[$j];
                                 }
-                                $dataArray[$field] = $data[$j];
-                            }
-                        } else {
-                            if (isset($data[$j]) && $data[$j]) {
-                                $dataArray[$extraField] .= "\n" . $field . ': ' . $data[$j];
+                            } else {
+                                if (isset($data[$j]) && $data[$j]) {
+                                    $dataArray[$extraField] .= "\n" . $field . ': ' . $data[$j];
+                                }
                             }
                         }
-                    }
-                    if (empty($dataArray[$extraField])) {
-                        unset($dataArray[$extraField]);
-                    }
-                    
-                    if (! empty($dataArray)) {
-                        $dataArray['users'] = ['_ids' => [$this->Auth->user('id')]];    //add myself as the contact person
-                        //debug($dataArray);
-                        $contact = $this->Contacts->newEntity($dataArray);
-                        $contact->loggedInUser = $this->Auth->user('id');
-                        $e = $contact->errors();
-                        if (empty($e)) {
-                            if ($this->Contacts->save($contact)) {
-                                $imported++;
+                        if (empty($dataArray[$extraField])) {
+                            unset($dataArray[$extraField]);
+                        }
+
+                        if (!empty($dataArray)) {
+                            $dataArray['users'] = ['_ids' => [$this->Auth->user('id')]];    //add myself as the contact person
+                            //debug($dataArray);
+                            $contact = $this->Contacts->newEntity($dataArray);
+                            $contact->loggedInUser = $this->Auth->user('id');
+                            $e = $contact->errors();
+                            if (empty($e)) {
+                                if ($this->Contacts->save($contact)) {
+                                    $imported++;
+                                } else {
+                                    $notImported++;
+                                    $errors[] = [
+                                        'errors' => $this->getErrors($contact->errors()),
+                                        'data' => $dataArray
+                                    ];
+                                }
                             } else {
                                 $notImported++;
                                 $errors[] = [
@@ -113,15 +133,12 @@ class ImportsController extends AppController
                                     'data' => $dataArray
                                 ];
                             }
-                        } else {
-                            $notImported++;
-                            $errors[] = [
-                                'errors' => $this->getErrors($contact->errors()),
-                                'data' => $dataArray
-                            ];
+                            unset($dataArray);
                         }
-                        unset($dataArray);
                     }
+                } else {
+                    $this->Flash->error(__('The import file was not a valid UTF-8 encoded csv file. Regenerate it.'));
+                    $this->Flash->error(__('Nothing is imported'));
                 }
                 $this->set('errors', $errors);
                 $this->set('fields', $fields);
@@ -129,6 +146,7 @@ class ImportsController extends AppController
                 $this->set('notImported', $notImported);
             } else {
                 $this->Flash->error(__('The import file was not in the proper format. Download the sample file and save as a csv.'));
+                $this->Flash->error(__('Nothing is imported'));
             }
         }
     }
