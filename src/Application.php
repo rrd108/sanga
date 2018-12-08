780e200efe4d24045db6e36a1cdbc7ec1be78a1e
rrd <rrd@1108.cc>
2018-12-08 18:54:26 +0100

    plugin loaded

--- src/Application.php
+++ src/Application.php
@@ -33,8 +33,6 @@ class Application extends BaseApplication
      */
     public function bootstrap()
     {
-        $this->addPlugin('cakephp-imagine-plugin');
-
         // Call parent to load bootstrap from files.
         parent::bootstrap();
     }
