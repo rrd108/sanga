9ab32caa355677f9dfefd1d5a91931322e568f8b
rrd <rrd@1108.cc>
2018-12-08 17:30:31 +0100

    cakephp-imagine-plugin plugin added

--- src/Application.php
+++ src/Application.php
@@ -33,6 +33,8 @@ class Application extends BaseApplication
      */
     public function bootstrap()
     {
+        $this->addPlugin('cakephp-imagine-plugin');
+
         // Call parent to load bootstrap from files.
         parent::bootstrap();
     }
